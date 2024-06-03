1009840c38425f8b21648b4a4a1041da658594f0
halfed <1half.ed@gmail.com>
2024-06-02 19:59:15 -0600

    adding react router basics

--- react-router-spa/src/pages/Home.js
+++ react-router-spa/src/pages/Home.js
@@ -1,11 +1,23 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 function HomePage() {
+  const navigate = useNavigate();
+
+  // useNavigate hook to programmatically call a route
+  function navigateHander() {
+    navigate('/products');
+  }
+  
+
   return (
-  <div>
+  <>
     <h1>My Home Page Sucka!</h1>
     <p>Go to <Link to="/products">the list of products</Link>.</p>
-  </div>
+
+    <p>
+        <button onClick={navigateHander}>Navigate</button>
+    </p>
+  </>
   )
 }
 
