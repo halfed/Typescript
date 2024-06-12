8ddddde1b3296fd4ad4e5d3d80ee3295232670bf
halfed <1half.ed@gmail.com>
2024-06-11 18:44:53 -0600

    building a react app with routing

---
+++ react-router-w-data-fetching/frontend/src/pages/EventNew.js
@@ -0,0 +1,13 @@
+import EventForm from "../components/EventForm";
+
+function EventsNew() {
+    
+
+    return (
+        <>
+            <EventForm method="post"/>
+        </>
+    )
+}
+
+export default EventsNew;
