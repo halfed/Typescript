4e1e8e285d0bf254d414be015020d9cfd520edb3
halfed <1half.ed@gmail.com>
2024-07-17 19:47:28 -0600

    Updating ts config code cleanup

--- type-script-drag-n-drop-project/src/app.ts
+++ type-script-drag-n-drop-project/src/app.ts
@@ -1,7 +1,3 @@
-
-// <reference path="components/project-input.ts" />
-// <reference path="components/project-list.ts" />
-
 import { ProjectInput } from "./components/project-input";
 import { ProjectList } from "./components/project-list";
 
@@ -15,4 +11,3 @@ import { ProjectList } from "./components/project-list";
 new ProjectInput();
 new ProjectList('active');
 new ProjectList('finished');
-// }
